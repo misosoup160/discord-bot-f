526a3c93eac074b7cf859ca00cff9e94ae80ca4b
miso160 <hotmisosoup160@gmail.com>
2021-06-02 11:35:57 +0900

    サーバーownerにadmin権限をつける

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,14 @@ class User < ApplicationRecord
     name = auth_hash[:info][:name]
     image_url = auth_hash[:info][:image]
     discriminator = auth_hash[:extra][:raw_info][:discriminator]
+    guild_info = Discordrb::API::Server.resolve('Bot ' + ENV['DISCORD_BOT_TOKEN'], ENV['DISCORD_SERVER_ID'], with_counts = nil)
+    owner_id = JSON.parse(guild_info)['owner_id']
 
     User.find_or_create_by!(provider: provider, uid: uid) do |user|
       user.name = name
       user.image_url = image_url
       user.discriminator = discriminator
+      user.admin = true if owner_id == uid
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_133429) do
+ActiveRecord::Schema.define(version: 2021_06_02_022807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_05_31_133429) do
     t.string "discriminator", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false, null: false
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
   end
 
