f25b45731fe6812a8e85ddc2e38a6c58d8372f3e
miso160 <hotmisosoup160@gmail.com>
2021-06-03 11:28:19 +0900

    rubocop修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Admin::UsersController < ApplicationController
   before_action :require_admin
 
--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -1,7 +1,9 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get admin_users_index_url
     assert_response :success
   end
