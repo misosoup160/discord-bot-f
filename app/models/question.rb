1f5b0f88dc60b3b901e6bdd333d79100b909af6c
miso160 <hotmisosoup160@gmail.com>
2021-06-03 19:41:10 +0900

    rubocop

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Question < ApplicationRecord
   validates :body, length: { maximum: 200 }, presence: true
 end
--- test/controllers/admin/questions_controller_test.rb
+++ test/controllers/admin/questions_controller_test.rb
@@ -1,4 +1,6 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class Admin::QuestionsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
