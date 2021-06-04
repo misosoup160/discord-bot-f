faf529b43c31575ae3c5ac7fbc2d2bbf21210b0d
miso160 <hotmisosoup160@gmail.com>
2021-06-04 13:10:43 +0900

    ruboco修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :answers
+  has_many :answers, dependent: :destroy
 
   def self.find_or_create_from_auth_hash!(auth_hash)
     provider = auth_hash[:provider]
--- test/controllers/answers_controller_test.rb
+++ test/controllers/answers_controller_test.rb
@@ -1,4 +1,6 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class AnswersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
