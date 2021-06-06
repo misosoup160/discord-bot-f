ce8bfdb2e3da83c0b8e598c23f46a35d4f70b26f
miso160 <hotmisosoup160@gmail.com>
2021-06-06 10:07:10 +0900

    answersテーブルにpostedカラムを追加

---
+++ db/migrate/20210606010238_add_posted_to_answers.rb
@@ -0,0 +1,5 @@
+class AddPostedToAnswers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :answers, :posted, :boolean, default: false, null:false 
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_04_005643) do
+ActiveRecord::Schema.define(version: 2021_06_06_010238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_06_04_005643) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "posted", default: false, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
   end
