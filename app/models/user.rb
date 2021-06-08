6517917be104855daa4444a72c386e6aa4cd9919
miso160 <hotmisosoup160@gmail.com>
2021-06-08 09:22:36 +0900

    サーバーのメンバー以外はログインできないコードをとりあえず追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,11 +14,15 @@ class User < ApplicationRecord
     guild_info = Discordrb::API::Server.resolve("Bot #{ENV['DISCORD_BOT_TOKEN']}", ENV['DISCORD_SERVER_ID'])
     owner_id = JSON.parse(guild_info)['owner_id']
 
-    User.find_or_create_by!(provider: provider, uid: uid) do |user|
-      user.name = name
-      user.image_url = image_url
-      user.discriminator = discriminator
-      user.admin = true if owner_id == uid
+    # サーバーメンバーではない場合リクエストに失敗してログインできない
+    menber = Discordrb::API::Server.resolve_member("Bot #{ENV['DISCORD_BOT_TOKEN']}", ENV['DISCORD_SERVER_ID'], uid)
+    if menber
+      User.find_or_create_by!(provider: provider, uid: uid) do |user|
+        user.name = name
+        user.image_url = image_url
+        user.discriminator = discriminator
+        user.admin = true if owner_id == uid
+      end
     end
   end
 
