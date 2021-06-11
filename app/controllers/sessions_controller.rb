aeff8b676a4cafb83553ec85e0de50e4e0de61df
miso160 <hotmisosoup160@gmail.com>
2021-06-11 17:33:10 +0900

    ログインした後ログインする前の画面にリダイレクトする

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,19 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth_hash!(request.env['omniauth.auth'])
     session[:user_id] = user.id
-    redirect_to root_path, notice: 'ログインしました'
+    redirect_back_or(root_path)
+    flash[:notice] = 'ログインしました'
   end
 
   def destroy
     reset_session
     redirect_to welcom_path, notice: 'ログアウトしました'
+  end
+
+  private
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
   end
 end
