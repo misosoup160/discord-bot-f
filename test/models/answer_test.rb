76b7df46263cdfbf2874e84bd619eb064e1e5946
miso160 <hotmisosoup160@gmail.com>
2021-06-15 11:42:43 +0900

    rubocop修正

--- test/models/answer_test.rb
+++ test/models/answer_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class AnswerTest < ActiveSupport::TestCase
-  test "#answers_search" do
+  test '#answers_search' do
     answer = answers(:one)
     assert_equal answer, Answer.search('焼肉定食').first
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,12 +3,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "#avatar_url" do
+  test '#avatar_url' do
     user = users(:alice)
     assert_equal 'http://127.0.0.1:3000/images/default_avatar.png', user.avatar_url
   end
 
-  test "#users_search" do
+  test '#users_search' do
     user = users(:alice)
     assert_equal user, User.search('alice#1234').first
   end
