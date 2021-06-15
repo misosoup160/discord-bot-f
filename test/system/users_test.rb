1b96a48a99e3fc7f8c7629dd71152aae57c09fee
miso160 <hotmisosoup160@gmail.com>
2021-06-15 09:56:15 +0900

    usersのテストを追加

---
+++ test/system/users_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class UsersTest < ApplicationSystemTestCase
+  test 'visiting the index' do
+    login_user users(:alice)
+    visit admin_users_url
+    assert_selector 'h1', text: 'ユーザー一覧'
+  end
+
+  test 'updating a user to admin' do
+    login_user users(:alice)
+    visit admin_users_url
+    click_link '管理者にする'
+    accept_confirm
+    assert_no_text '管理者にする'
+  end
+end
