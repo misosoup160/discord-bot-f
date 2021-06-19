2874ae24c969b6bd85ba80c1f9307605fde90434
miso160 <hotmisosoup160@gmail.com>
2021-06-19 10:53:19 +0900

    textareaの改行を２文字でカウントするようにする

--- app/javascript/packs/answer_text_count.js
+++ app/javascript/packs/answer_text_count.js
@@ -2,11 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const maxCount = 2000
   const inputArea = document.getElementById('answer_body')
   const counter = document.getElementById('answer_counter')
-  let inputCharCount = inputArea.value.length || 0
+  const rawValue = inputArea.value
+  const crlf = rawValue.match(/(\r\n|\n|\r)/g)
+  const crlfCount = crlf ? crlf.length : 0
+  const inputCharCount = inputArea.value.length + crlfCount
   counter.innerHTML = maxCount - inputCharCount
 
   inputArea.onkeyup = () => {
-    let inputCharCount = inputArea.value.length || 0
+    const rawValue = inputArea.value
+    const crlf = rawValue.match(/(\r\n|\n|\r)/g)
+    const crlfCount = crlf ? crlf.length : 0
+    const inputCharCount = inputArea.value.length + crlfCount
     counter.innerHTML = maxCount - inputCharCount
     if (maxCount - inputCharCount < 0) {
       counter.style.color = '#fdad10'
--- app/javascript/packs/question_text_count.js
+++ app/javascript/packs/question_text_count.js
@@ -2,11 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const maxCount = 200
   const inputArea = document.getElementById('question_body')
   const counter = document.getElementById('question_counter')
-  let inputCharCount = inputArea.value.length || 0
+  const rawValue = inputArea.value
+  const crlf = rawValue.match(/(\r\n|\n|\r)/g)
+  const crlfCount = crlf ? crlf.length : 0
+  const inputCharCount = inputArea.value.length + crlfCount
   counter.innerHTML = maxCount - inputCharCount
 
   inputArea.onkeyup = () => {
-    let inputCharCount = inputArea.value.length || 0
+    const rawValue = inputArea.value
+    const crlf = rawValue.match(/(\r\n|\n|\r)/g)
+    const crlfCount = crlf ? crlf.length : 0
+    const inputCharCount = inputArea.value.length + crlfCount
     counter.innerHTML = maxCount - inputCharCount
     if (maxCount - inputCharCount < 0) {
       counter.style.color = '#fdad10'
