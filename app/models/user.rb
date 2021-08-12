4cda2db8b0d00082990033ca176504c13ad23776
miso160 <hotmisosoup160@gmail.com>
2021-08-12 15:00:31 +0900

    ログイン時のアバター登録を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
     provider = auth_hash[:provider]
     uid = auth_hash[:uid]
     name = auth_hash[:info][:name]
-    avatar = auth_hash[:info][:image]
+    avatar = auth_hash[:extra][:raw_info][:avatar]
     discriminator = auth_hash[:extra][:raw_info][:discriminator]
     guild_info = Discordrb::API::Server.resolve("Bot #{ENV['DISCORD_BOT_TOKEN']}", ENV['DISCORD_SERVER_ID'])
     owner_id = JSON.parse(guild_info)['owner_id']
@@ -19,7 +19,9 @@ class User < ApplicationRecord
 
     User.find_or_create_by!(provider: provider, uid: uid) do |user|
       user.name = name
-      user.avatar = avatar
+      user.avatar = avatar ?
+        Discordrb::API::User.avatar_url(uid, avatar) :
+        Discordrb::API::User.default_avatar(discriminator)
       user.discriminator = discriminator
       user.admin = true if owner_id == uid
       user.owner = true if owner_id == uid
